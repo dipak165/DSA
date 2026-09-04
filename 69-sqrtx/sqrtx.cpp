e33c98f0c8c9ae8f8ce233395e089a2de61e4e5c
Dipak Shinde <1shindedipak@gmail.com>
2026-09-04 15:58:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (51.07%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x < 2) {
+            return x;
+        }
+
+        int left = 1;
+        int right = x / 2;
+        int answer = 0;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            long square = (long) mid * mid;
+
+            if (square == x) {
+                return mid;
+            }
+
+            if (square < x) {
+                answer = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return answer;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
