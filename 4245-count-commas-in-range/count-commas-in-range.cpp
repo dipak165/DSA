3a3041a75ae044a78bfa787be4b3ec062df76058
Dipak Shinde <1shindedipak@gmail.com>
2026-09-10 00:19:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (51.72%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        if(n<999){
+            return 0;
+        }
+
+        int ans=n-999;
+        return ans;
+        
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
